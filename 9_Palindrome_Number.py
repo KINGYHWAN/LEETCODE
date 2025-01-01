3ba6a94d5ca0dd07b3d9ed2c70649929357d7299
KINGYHWAN <ghks5345@hanyang.ac.kr>
2025-01-01 16:49:06 +0900

    [LEETCODE] Palidrome Number Practice Code

---
+++ 9_Palindrome_Number.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        rev = str(x)[::-1]
+
+        :type x: int
+        :rtype: bool
+        """
+        x_list = list(map(str, str(x)))
+        rtype = True
+        for i in range(len(x_list)):
+            if x_list[i] != x_list[len(x_list)-i-1]:
+                rtype = False
+
+        return rtype  
+
+        
