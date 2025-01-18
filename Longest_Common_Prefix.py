8dd06d06d5b0d8b074058dab4ed7b9311e31fada
KINGYHWAN <ghks5345@hanyang.ac.kr>
2025-01-18 11:37:30 +0900

    [ADD] #4

---
+++ Longest_Common_Prefix.py
@@ -0,0 +1,53 @@
+# solution 1
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+
+        if not strs:
+            return ""
+
+        new = ""
+        strs = sorted(strs)
+        first, last = strs[0], strs[-1]
+        for i in range(len(first)):
+            if i < len(last) and first[i] == last[i]:
+                new += first[i]
+            else:
+                break
+        
+        return new
+
+# solution 2
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        prefix = ""
+        track = False
+
+        for i in range(len(min(strs, key = len))):
+            for j in range(1, len(strs)):
+                if strs[0][i] == strs[j][i]:
+                    continue
+                else:
+                    track = True
+                    break
+
+            if track == False:
+                prefix += strs[0][i]
+            
+            if bool(track):
+                break
+
+        return prefix 
+
+
+
+
+
