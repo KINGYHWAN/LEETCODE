5d879cf70144a59bd4b6f482780b094c25146dc1
KINGYHWAN <ghks5345@hanyang.ac.kr>
2025-04-12 18:35:14 +0900

    [LEETCODE] #199

---
+++ 199_Binary_Tree_Right_Side_View.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        
+        if not root:
+            return []
+
+        result = []
+
+        level = [root]
+
+        while level:
+            result.append(level[-1].val)
+            temp = []
+            for i in level:
+                if i.left:
+                    temp.append(i.left)
+                if i.right:
+                    temp.append(i.right)
+            
+            level = temp.copy()
+        
+        return result
+
+
+                
+
