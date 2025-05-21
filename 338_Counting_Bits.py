6063e2ba79ac47074aced771afac555f7ffabfd0
KINGYHWAN <ghks5345@hanyang.ac.kr>
2025-05-21 11:07:58 +0900

    [LEETCODE] #338

---
+++ 338_Counting_Bits.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        
+        """
+        
+        n = 5 -> 101
+
+        i >> 1 -> 10 -> 2 -> ans[i >> 1] = 1
+        i & 1 = 1(odd), 0(even)
+        
+        """
+        ans = [0] * (n+1)
+        for i in range(1, n+1):
+            ans[i] = ans[i >> 1] + (i & 1)
+
+        return ans
